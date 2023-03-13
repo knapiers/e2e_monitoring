2394d8719f55623c1d0b06a8081150a5280317a6
Kacper Napierski <kacper.napierski@intel.com>
2023-03-13 14:21:45 +0000

    Logging added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import json
+import logging
 from getpass import getpass
 import sys
 import device_list as list
 from paramiko import SSHClient, AutoAddPolicy
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_file_handler = logging.FileHandler(f'{__name__}.log', mode="w")
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s") # FIXME time of a log 
+log_file_handler.setFormatter(formatter)
+logger.addHandler(log_file_handler)
 
 
 def get_credentials():
@@ -21,30 +30,38 @@ def main():
 
     with open("output.txt", "w") as file:
         for nuc_name, nuc_ip in list.device_list.items():
-            print(f"Connecting=={nuc_name}=={nuc_ip}")
-            client.connect(nuc_ip, username=username, password=password)
-            print(f"Connected=={nuc_name}=={nuc_ip}")
+
+            logger.info(f"Connecting {nuc_name} {nuc_ip}")
+            try:
+                client.connect(nuc_ip, username=username, password=password)
+            except Exception:
+                logger.exception(f"Cannot connect to {nuc_name} {nuc_ip}")
 
             file.write(f"\n\n======{nuc_name}=={nuc_ip}======")
 
-            file.write("\n===DOCKER STATUS===\n")
-            stdout1 = client.exec_command("sudo systemctl status docker | head -4 | grep 'Active'")[1]
-            file.write(stdout1.read().decode('utf8'))
-
-
+            try:
+                file.write("\n===DOCKER STATUS===\n")
+                stdout1 = client.exec_command("sudo systemctl status docker | head -4 | grep 'Active'")[1]
+                file.write(stdout1.read().decode('utf8'))
+            except Exception:
+                logger.exception(f"No Active docker {nuc_name} {nuc_ip}")
+            
             file.write("\n===DISK STATUS===\n")
             stdout2 = client.exec_command("df -h")[1]
             file.write(stdout2.read().decode('utf8'))
 
-            file.write("\n===LINKING STATUS===\n")
-            stdout3 = client.exec_command("sudo docker ps -a | awk '{print $1}' | awk 'length($1) >10'")[1]
-            container = stdout3.read().decode('utf8')
-            stdout4 = client.exec_command(f"sudo docker inspect {container}")[1]
-            inspect_string = stdout4.read().decode('utf8')
-            json_object = json.loads(inspect_string)
-            file.write(json.dumps(json_object[0]['Args'], indent=3))
+            try:
+                file.write("\n===LINKING STATUS===\n")
+                stdout3 = client.exec_command("sudo docker ps -a | awk '{print $1}' | awk 'length($1) >10'")[1]
+                container = stdout3.read().decode('utf8')
+                stdout4 = client.exec_command(f"sudo docker inspect {container}")[1]
+                inspect_string = stdout4.read().decode('utf8')
+                json_object = json.loads(inspect_string)
+                file.write(json.dumps(json_object[0]['Args'], indent=3))
+            except Exception:
+                logger.exception(f"No Docker to inspect {nuc_name} {nuc_ip}")
 
-            print(f"Done=={nuc_name}=={nuc_ip}")
+            logger.info(f"Done {nuc_name} {nuc_ip}")
 
 
 if __name__ == "__main__":
